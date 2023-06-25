d97b9418ac5bcf504b63419609dc9cfb116e2888
Becasuse-hung <4a33a006@stust.edu.tw>
2023-06-25 19:17:09 +0800

    feat: 測試 table key 值

---
+++ src/api/fakeApi.js
@@ -0,0 +1,100 @@
+export const fetchFakeApi = () => {
+  return new Promise((resolve) => {
+    const res = {
+      data: {
+        columns: [
+          {
+            name: 'tom17',
+            country: 'USA',
+            id: 'live',
+            age: 22,
+            food: [
+              {
+                id: 153252,
+                cookie: 'straw'
+              },
+              {
+                id: 744138,
+                cookie: 'banana'
+              },
+              {
+                id: 184312,
+                cookie: 'menlon'
+              }
+            ]
+          },
+          {
+            name: 'bay12',
+            country: 'CANA',
+            id: 'fixtures',
+            food: [
+              {
+                id: 102482,
+                cookie: 'straw'
+              },
+              {
+                id: 407782,
+                cookie: 'water'
+              },
+              {
+                id: 889782,
+                cookie: 'coden'
+              },
+              {
+                id: 482187,
+                cookie: 'beer'
+              },
+              {
+                id: 105538,
+                cookie: 'queen'
+              }
+            ]
+          },
+          {
+            name: 'Kom',
+            country: 'lorea',
+            id: 'finish',
+            age: 20,
+            food: [
+              {
+                id: 102738,
+                cookie: 'stw'
+              },
+              {
+                id: 780457,
+                cookie: 'kta'
+              }
+            ]
+          },
+          {
+            name: 'pp',
+            country: 'USAaaa',
+            id: 'others',
+            age: null,
+            food: [
+              {
+                id: 102239,
+                cookie: 'sneak'
+              },
+              {
+                id: 402493,
+                cookie: 'fgh'
+              },
+              {
+                id: 987662,
+                cookie: 'tty'
+              },
+              {
+                id: 569732,
+                cookie: 'skll'
+              }
+            ]
+          }
+        ]
+      }
+    }
+    setTimeout(() => {
+      resolve(res)
+    }, 2000)
+  })
+}
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,48 @@
 <template>
-  <div class="about">
+  <div class='about'>
     <h1>This is an about page</h1>
+    <div>
+      <Table :columns='columns' :data='deta'></Table>
+  </div>
   </div>
 </template>
+<script>
+import { fetchFakeApi } from '../api/fakeApi'
+export default {
+  name: 'HomeView',
+  data () {
+    return {
+      deta: [],
+      columns: [
+        {
+          title: 'Name',
+          key: 'name'
+        },
+        {
+          title: 'Country',
+          key: 'country'
+        },
+        {
+          title: 'Id',
+          key: 'id'
+        },
+        {
+          title: 'Age',
+          key: 'age'
+        }
+      ]
+    }
+  },
+  methods: {
+    getData () {
+      fetchFakeApi().then((res) => {
+        console.log('res', res)
+        this.deta = res.data.columns
+      })
+    }
+  },
+  mounted () {
+    this.getData()
+  }
+}
+</script>
