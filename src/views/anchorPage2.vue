c8500a2298518727024bb0bd28a2834929fb1c06
Becasuse-hung <4a33a006@stust.edu.tw>
2023-12-20 23:47:04 +0800

    feat: simple scrollspy

--- src/views/anchorPage2.vue
+++ src/views/anchorPage2.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class='container'>
+    <div class='container'>
+    <div class='list'>
+      <div
+        :class='{ longHeight: long && Number(item.id) % 2 }'
+        class='item scrollspy'
+        :id='`${item.id}-id`'
+        v-for='(item, i) in Ilist'
+        :key='i'
+      >
+        {{ item.id }}
+      </div>
+    </div>
+    <ul class='menu' id="menu">
+        <a
+        class='anchor'
+        :id='item.id'
+        v-for='(item, i) in Ilist'
+        :key='i'
+        :href='`#${item.id}-id`'
+        >item {{ item.id }}</a
+        >
+    </ul>
+  </div>
+  <!-- <div class='container'>
     <div class='list' v-scroll-spy="{offset: 150}">
       <div
         :class='{ longHeight: long && Number(item.id) % 2 }'
@@ -11,19 +34,20 @@
         {{ item.id }}
       </div>
     </div>
-    <div class='menu' id="menu"  v-scroll-spy-active="{selector: 'a.anchor', class: 'active'}">
-      <a
+    <ul class='menu' id="menu"  v-scroll-spy-active="{selector: 'a.anchor', class: 'active'}">
+        <a
         class='anchor'
         :id='item.id'
         v-for='(item, i) in Ilist'
         :key='i'
         :href='`#${item.id}-id`'
         >item {{ item.id }}</a
-      >
-    </div>
-  </div>
+        >
+    </ul>
+  </div> -->
 </template>
 <script>
+import scrollSpy from 'simple-scrollspy'
 export default {
   data () {
     return {
@@ -63,6 +87,17 @@ export default {
     }
   },
   mounted () {
+    window.addEventListener('load', () => {
+      const options = {
+        sectionClass: '.scrollspy', // Query selector to your sections
+        menuActiveTarget: '.anchor', // Query selector to your elements that will be added `active` class
+        offset: 500, // Menu item will active before scroll to a matched section 100px
+        smoothScroll: true,
+        activeClass: 'activeLi'
+      }
+
+      scrollSpy('#menu', options)
+    })
   }
 }
 </script>
@@ -106,7 +141,7 @@ a {
 }
 
 a{
-  &.active{
+  &.activeLi{
     color: #f00;
   }
 }
