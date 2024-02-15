2e287e9c314a21653ac10a6a8e4e667fdbb252d5
Becasuse-hung <4a33a006@stust.edu.tw>
2024-02-15 23:43:56 +0800

    feat: 圖片 text-align center 置中 + css has 選擇器

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import anchor2 from '../views/anchorPage2.vue'
 import vantTest from '../views/vantTest.vue'
 import vantSw from '../views/vantSwiper.vue'
 import phone from '../views/phoneNumber.vue'
+import testImg from '../views/testImg.vue'
 
 Vue.use(VueRouter)
 
@@ -24,6 +25,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/testImg',
+    name: 'testImg',
+    component: testImg
   },
   {
     path: '/gamelist',
---
+++ src/views/testImg.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <p>
+      title
+    </p>
+    <p>
+      <img src="../assets/fish.jpg" alt="">
+    </p>
+    <p>
+      <span><img src="../assets/fish.jpg" alt=""></span>
+    </p>
+  </div>
+</template>
+<style lang="scss" scoped>
+p{
+  width: 1200px;
+  height: 250px;
+  border: 1px solid red;
+  text-align: center;
+
+  img{
+    display: inline-block;
+    max-width: 100%;
+    height: auto;
+    object-fit: contain;
+  }
+}
+p:has(img){
+  text-align: left;
+  border: 1px solid yellow;
+}
+
+p:has( > img){
+  text-align: left;
+  border: 1px solid blue;
+}
+
+</style>
