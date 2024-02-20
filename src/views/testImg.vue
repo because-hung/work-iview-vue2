0f9241dee23fbd64bc968fb23c2822b8c16e1018
Becasuse-hung <4a33a006@stust.edu.tw>
2024-02-20 22:21:28 +0800

    feat: telegram 正則判斷

--- src/views/testImg.vue
+++ src/views/testImg.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <h2>TG regex</h2>
     <p>
       title
     </p>
@@ -36,3 +37,24 @@ p:has( > img){
 }
 
 </style>
+<script>
+export default {
+  name: 'testImg',
+  data () {
+    return {
+      url: 'https://t.me/barry_hong',
+      url2: 'https://telegram.me/joinchat/B-UUVD8-WVj8KtAysJzIiw'
+    }
+  },
+  methods: {
+    regexTG () {
+      const reg1 = /(?:t\.me|telegram\.me)/
+      console.log('res1', reg1.test(this.url))
+      console.log('res2', reg1.test(this.url2))
+    }
+  },
+  mounted () {
+    this.regexTG()
+  }
+}
+</script>
